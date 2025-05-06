ef218880586d1e12b301f9e711562116b776da39
javadmohebbi <mohammadjavadmohebbi@gmail.com>
2025-05-06 11:01:35 -0500

    go doc

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
-// main.go
+// Command goiam starts the goIAM identity and access management API server.
+// This is the top-level entry point that delegates startup to the internal server package.
 package main
 
 import "github.com/javadmohebbi/goIAM/cmd/server"
 
+// main initializes and starts the goIAM server using the cmd/server package.
 func main() {
 	server.Main()
 }

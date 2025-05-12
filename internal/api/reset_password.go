e9cb7e1b3783e6a9b36042a6dc6d1fd8b6723e3a
javadmohebbi <mohammadjavadmohebbi@gmail.com>
2025-05-12 15:57:37 -0500

    passwprd reset template

--- internal/api/reset_password.go
+++ internal/api/reset_password.go
@@ -1,11 +1,15 @@
 package api
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/javadmohebbi/goIAM/internal/db"
+)
 
 // handleResetPasswordRequestLocal send the instruction to user to reset his password
 func (a *API) handleResetPasswordRequestLocal(c fiber.Ctx) error {
 	type req struct {
 		Username string `json:"username"` // required
+		Email    string `json:"email"`    // required
 	}
 
 	var body req
@@ -13,5 +17,26 @@ func (a *API) handleResetPasswordRequestLocal(c fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, "invalid input")
 	}
 
-	// return nil
+	var user db.User
+
+	// if username is provided
+	if body.Username != "" {
+		if err := a.iamDB.Model(&db.User{}).Where("username = ?", body.Username).First(&user); err != nil {
+			// log error
+		}
+		return nil
+	}
+
+	// if email provided
+	if body.Email != "" {
+		if err := a.iamDB.Model(&db.User{}).Where("username = ?", body.Username).First(&user); err != nil {
+			// log error
+		}
+		return nil
+	}
+
+	// anyway, we will show a message to user
+	// that if the provided info is correct,
+	// you will recieve an email to reset your password
+	return nil
 }

1a65d968b141353616bc47c97ade7fdd7bea7fe3
javadmohebbi <mohammadjavadmohebbi@gmail.com>
2025-05-12 15:13:50 -0500

    Random string generator

---
+++ internal/utils/utils.go
@@ -0,0 +1,16 @@
+package utils
+
+import "crypto/rand"
+
+func GenerateRandomString(length int) (string, error) {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-"
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	for i, b := range bytes {
+		bytes[i] = charset[b%byte(len(charset))]
+	}
+	return string(bytes), nil
+}
